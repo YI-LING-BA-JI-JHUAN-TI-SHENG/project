81bcb98183ae3bc81d2c4f913d871aba94f0d731
Shih-Yen-Shih <aben20807@gmail.com>
2018-09-02 15:14:27 +0800

    Remove profile and add indent

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
-gnome-terminal --window --maximize \
-    --tab -t "roscore"  --profile=OuO -e 'bash -c "roscore"' \
-    --tab -t "ev3"      --profile=OuO -e 'bash -c "sleep 5; ssh root@192.168.1.101"' \
-    --tab -t "docker"   --profile=OuO --working-directory=$HOME/rosev3/gripp3r/ \
+gnome-terminal \
+    --window --maximize \
+        -e 'bash -c "echo \"sh launch_all.sh\"; \
+            bash"' \
+    --tab -t "roscore" \
+        -e 'bash -c "roscore"' \
+    --tab -t "ev3" \
+        -e 'bash -c "sleep 5; \
+            echo \"ev3_manager\"; \
+            ssh root@192.168.1.101"' \
+    --tab -t "docker" --working-directory=$HOME/rosev3/gripp3r/ \
         -e 'bash -c "sleep 5; sudo sh down.sh; sudo sh rosini.sh"'
